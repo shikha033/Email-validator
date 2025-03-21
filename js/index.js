744a4257bddad4173bdb8fd108728ec3b60f214a
shikha033 <shikhasingh242494@gmail.com>
2025-03-21 23:17:29 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 console.log("This is my script")
 
-
-
-
 let result = {
     "tag": "",
     "free": false,
@@ -26,12 +23,14 @@ let result = {
 submitBtn.addEventListener("click", async (e) => {
     e.preventDefault()
     console.log("Clicked!")
-    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    resultCont.innerHTML = `<img width="100" src="images/loading.svg" alt="Loading...">`
     let key = "ema_live_ITkV5Pqq40WNdMLdjqMZlNcb4AEBrNvWiSFd1MYE"
     let email = document.getElementById("username").value 
-    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    //let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`;
     let res = await fetch(url)
     let result = await res.json()
+    
     let str = ``
     for (key of Object.keys(result)) {
         if(result[key] !== "" && result[key]!== " "){ 
@@ -42,3 +41,4 @@ submitBtn.addEventListener("click", async (e) => {
     console.log(str)
     resultCont.innerHTML = str
 })
+
